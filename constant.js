6c3cc158cb1d23d59086bd6ae483db5476a76811
Ha Nguyen <hnn152151927@gmail.com>
2023-02-14 17:50:48 +0700

    update lorelist

--- constant.js
+++ constant.js
@@ -32,7 +32,179 @@ export const initLore = [
     "search": "bò là ai",
     "lore": "xúk vật xứ 34, chuyên đâm chọc chia rẽ anh em xong bị gọi tên thì lẩn",
     "capture": "https://media.discordapp.net/attachments/392248883436322828/882111265630212137/unknown.png"
-  }
+  },
+  {
+    "search": "thằng 79",
+    "lore": "",
+    "capture": "https://media.discordapp.net/attachments/882451550369370132/973407885419741204/unknown.png"
+  },
+  {
+    "search": "tôm là ai",
+    "lore": "404 Not Found...",
+  },
+  {
+    "search": "nam là ai",
+    "lore": "Hỏi Nam ôn thần nake, chúa tể tư vấn hay Nam full skins <:ngr:421524933781356546>",
+  },
+  {
+    "search": "nam đần là ai",
+    "lore": "Kẻ làm chủ hơi thở của quỷ, Kẻ thù của loài Bò, từng book vé ra 34 để xiên Thợ Ảnh nhưng không thành, thường giận hờn vu vơ, tốt tính hay tư vấn những pha đi vào lòng đất <:pphe:1074997214964895874>",
+  },
+  {
+    "search": "mén là ai",
+    "lore": `Mén - Tein- Thách đấu fake - người nên tìm kiếm vào cuối mùa\nTrùm loser, tán đứa nào đứa đấy có người yêu ngay sau đó mà không phải là mén.\nLà một thanh niên biết cam chịu, đầu đội sừng, chân đạp vỏ.  Hiên ngang ̶c̶̶h̶̶ế̶̶t̶ đứng giữa đất trời <:sure:1075000696706977822>`,
+  },
+  {
+    "search": "minh là ai",
+    "lore": "Minh hay Phúc thì đó vẫn là 1 ẩn số nhưng có 1 sự thật là xv nhà giàu giả chết - Chuyên xem stream chùa không donate. Ngoài ra  cái tên này được thay cho tính từ chỉ sự occhos hoặc bộ phận sinh dục nữ. VD: sao mày minh thế? Cười tụt minh <:cuoiminh:1075000861522149427>",
+  },
+  {
+    "search": "nhi là ai",
+    "lore": "Tiểu thư Sài Thành - sở hữu bộ sưu tập chuối mà bất cứ cô gái nào cũng mơ ước",
+  },
+  {
+    "search": "sú là ai",
+    "lore": "Có thể gọi là nhà cơ to - giàu nhất nhì clan, con bố tướng,  nhưng không vì thế mà lười biếng, anh hiện tại đang là coder nghìn đô, cùng với đó, anh có một bộ sưu tập ảnh gái xinh cấp 2, cấp 3 mơn mởn, hãy gọi anh là ANH SÚ, nếu chửi xin hãy chửi là CONĐĨANHSÚ <:quada:1075001101499252736>",
+  },
+  {
+    "search": "việt là ai",
+    "lore": "Việt Malphite: Đại gia Time city, tốt nghiệp đại học ngành giả chết https://youtu.be/kUKcWKQkGd0",
+  },
+  {
+    "search": "yu là ai",
+    "lore": "Quận chúa Bắc Linh - Park sĩ thất nghiệp - làm vì đam mê vì nghỉ việc rồi ai mà trả lương <:sosad:1075001392621703268>",
+  },
+  {
+    "search": "huy là ai",
+    "lore": "Làm cóc cho người ta mén, Làm huy cho yu nó trêu",
+    "capture": "https://media.discordapp.net/attachments/392248883436322828/981480972757385226/unknown-3-1.png"
+  },
+  {
+    "search": "zane là ai",
+    "lore": `Zane - Nhà tìnk rục học <:sexy:1075001593742762004>\nNếu một ngày bạn mở mắt ra và thấy mình đã là người tối cổ. Hãy tìm gặp ngay như like fật tổ Zane để thỉnh chân kink bản mới nhất đem về tu luyện <:stick:1075001670586597426>`,
+  },
+  {
+    "search": "cóc là ai",
+    "lore": "IT vua nghề, ăn sáng 1 củ nhưng vẫn thích giả chết unseen",
+  },
+  {
+    "search": "đức là ai",
+    "lore": "Đức Thảo Điền, chúa tể console, ông hoàng game offline, ngoài thời gian ăn, ỉa, ngủ thì ngày cày game 25/24h, còn dư 1h lên discord xiaolon đặt điều",
+  },
+  {
+    "search": "cú là ai",
+    "lore": "Cán bộ ngành xây dựng, người thuần hóa sư tử Yu đừn, đặc biệt là có người yêu mà còn... ",
+    "capture": "https://media.discordapp.net/attachments/392248883436322828/1049602104311959612/unknown.png"
+  },
+  {
+    "search": "vũ là ai",
+    "lore": "Đẹp trai sợ vợ, hay afk đi xúc cứt cho mèo, rửa đít cho con, địa vị trong nhà thuộc Top 4 đứng sau Con vợ, con trai và con mòe ... thường bị cà cắn vào đít <:sadd:1075001956373905408>",
+  },
+  {
+    "search": "chen là ai",
+    "lore": "Chen 3k - ông vua Malzahar smr, kẻ hiền nhất nake, tuy nhiên",
+    "capture": "https://media.discordapp.net/attachments/392248883436322828/1066751329889955910/unknown.png"
+  },
+  {
+    "search": "xoá là ai",
+    "lore": "sinh ra từ cái nôi cách mạng, uống bia thay cơm, vì gái bỏ bạn, yêu lửa chùa phản bội liên minh, thầy giáo dục công dân, ông tổ đạo lý",
+  },
+  {
+    "search": "xóa là ai",
+    "lore": "sinh ra từ cái nôi cách mạng, uống bia thay cơm, vì gái bỏ bạn, yêu lửa chùa phản bội liên minh, thầy giáo dục công dân, ông tổ đạo lý",
+  },
+  {
+    "search": "khá là ai",
+    "lore": "Bắc Ánh, nam Khá. Biệt danh Khá Không Ngủ, nổi danh trong làng địt dạo, khách hàng lớn của Tinder, ăn nhậu vô đối xứ Nake, sở thích là đớp GA và đấm Mèo.",
+  },
+  {
+    "search": "trẩu là ai",
+    "lore": "Xúc xinh bỏ bạn giữa bầy địch :doubt~1: , Chuyên gia ăn bánh. Bằng chứng cho khỏi cãi: https://www.youtube.com/watch?v=VVHh4z7QalU",
+  },
+  {
+    "search": "bét ad là ai",
+    "lore": "Rola Takizawa - Best AD f253 - Bét AD - Chúa tể của băng trụ-  Đỉnh cao Teemo - Bậc thầy núp bụi https://www.youtube.com/watch?v=0eQU0SLTptU",
+  },
+  {
+    "search": "ánh là ai",
+    "lore": "máy dập ba đình, chúa tể fwb, ông hoàng ons, vị vua trên giường, thợ địt nước úc, thạc sĩ tình dục, thợ đục năm châu",
+    "capture": "https://media.discordapp.net/attachments/392248883436322828/1067428922658148383/20211030_144427.png"
+  },
+  {
+    "search": "đúng là thứ",
+    "lore": "Thứ này hả <:look_down:1075002586589036596>",
+    "capture": "https://media.discordapp.net/attachments/392248883436322828/969590336240357376/11.png"
+  },
+  {
+    "search": "ngu này",
+    "lore": "https://media.discordapp.net/attachments/392248883436322828/1069116743945031751/lx3TBCA.gif",
+  },
+  {
+    "search": "thằng tên huy",
+    "lore": "toàn xúc vật - 2k said",
+  },
+  {
+    "search": "thằng tên việt",
+    "lore": "toàn chối bỏ bản thân",
+  },
+  {
+    "search": "8 chữ vàng",
+    "lore": "tranh công - chối tội - đổ lỗi - thanh minh <:doubt:1075002836372430888>",
+  },
+  {
+    "search": "tam đại chó",
+    "lore": `1. Bake Củ đụ <:doge:1075002969105367121> \n2. Truke Hột cà <:doge:1075002969105367121> \n3. Nake Minh phốc <:doge:1075002969105367121>`
+  },
+  {
+    "search": "tứ đại ngu",
+    "lore": `1: Tfuck <:sogood:1075003123153780776>\n2: Bô <:sogood:1075003123153780776> \n3: Vẩu <:sogood:1075003123153780776> \n4: Penis <:sogood:1075003123153780776>`
+  },
+  {
+    "search": "thằng tên trung",
+    "lore": "https://media.discordapp.net/attachments/392248883436322828/970926009958420480/unknown.png",
+  },
+  {
+    "search": "tfuck là ai",
+    "lore": "Trung trong Trung Địa, Mọt dân BTC  , Đứng đầu trong Tứ đại ngu, kêu lương 4 củ mà tháng tiêu 50 củ, mai mối đéo có tâm , khách hàng 1 sao. ",
+    "capture": "https://media.discordapp.net/attachments/750910417521475674/954637538545520640/unknown.png"
+  },
+  {
+    "search": "loan là ai",
+    "lore": "Loan hay còn gọi là loandet cầm tinh con cá mút đá với skill  thần cấp bú bòi , bú bòi mỗi khi vui, buồn hoặc khi không vui chẳng buồn, IT 96 bách khoa, 2k$, là loan cũng là việt, giàu mà ngu",
+    "capture": "https://media.discordapp.net/attachments/392248883436322828/1068113699644112906/unknown.png?width=375&height=670"
+  },
+  {
+    "search": "giang là ai",
+    "lore": "Giang bêđê hay còn được gọi là Jiang công túa aka Jang Đẫm, là ''Sông Dài'' xứ Lạng nên rất Uốn Éo,  thân xác đàn ông nhưng lại mang trong mình bộ nhiễm sắc thể XX, một trong tứ đại mọt dân lương ba củ khóc rất to nhưng đéo chịu bỏ việc . Cán bộ chuyên trap dân.",
+  },
+  {
+    "search": "1 cơ hội",
+    "lore": "",
+    "capture": "https://cdn.discordapp.com/attachments/882451550369370132/957956780820922398/unknown.png"
+  },
+  {
+    "search": "thằng tên đức",
+    "lore": "",
+    "capture": "https://media.discordapp.net/attachments/882451550369370132/955743907180863569/519BA6C6-B2DB-411F-B8B5-43FF776393B7.png?width=881&height=551"
+  },
+  {
+    "search": "joley là ai",
+    "lore": "Joley - Jolon - Tony đầu khấc - Quân Bêđê quận 12",
+  },
+  {
+    "search": "gì căng",
+    "lore": "Đúng rồi, làm gì mà căng <:doge:1075002969105367121> ",
+  },
+  {
+    "search": "mai là ai",
+    "lore": "Mai - Mice - Mile - Mòe ",
+    "capture": "https://images-ext-1.discordapp.net/external/J9q0kjc90qLXaCgZelBdaPiEZbiMRJou3XUbk_4HsDo/https/i.imgur.com/KxLaFe3.png"
+  },
+  {
+    "search": "do anything",
+    "lore": "KWANG do everything",
+    "capture": "<:pmc1:1075004166382034985>"
+  },
 ]
 
 export const initLogs = [
